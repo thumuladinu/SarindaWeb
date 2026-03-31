fe75055d5f1e777d224d298a32a36e7c61252a92
thumuladinu <brothercoc11@gmail.com>
2026-03-31 09:34:05 +0530

    [C[D

--- frontend-modern/dev-dist/sw.js
+++ frontend-modern/dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.seal0eqfi8g"
+    "revision": "0.n3ni251hom8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
